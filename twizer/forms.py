314e3b3c4282bc81875c183ec90e3d77762bf18a
vendari12 <54756063+vendari12@users.noreply.github.com>
2021-08-12 04:48:23 +0100

    Update forms.py

--- twizer/forms.py
+++ twizer/forms.py
@@ -4,15 +4,17 @@ from django import forms
 
 
 class DataForm(forms.Form):  
-    #filename = forms.CharField(label="Enter file name", max_length=50)
+    filename = forms.CharField(label="Enter file name", max_length=50)
     file = forms.FileField()
-    def clean(self):
-        data = self.cleaned_data.get('file')
-        super(DataForm, self).clean()
-        if data:
-            filename = data
-            ext = os.path.splitext(filename)[1]
-            ext = ext.lower()
-            if ext != '.csv':
-                raise forms.ValidationError("Filetype not allowed!")
-        return self.cleaned_data
+
+    #file extension validation
+    #def clean(self):
+     #   data = self.cleaned_data.get('file')
+        #super(DataForm, self).clean()
+        #if data:
+            #filename = data
+            #ext = os.path.splitext(data)[1]
+            #ext = ext.lower()
+            #if ext != '.csv':
+             #   raise forms.ValidationError("Filetype not allowed!")
+        #return self.cleaned_data
