8ed751ea09da6f3f40c6195168dedfd27e4ad114
vendari12 <54756063+vendari12@users.noreply.github.com>
2021-08-12 04:47:15 +0100

    Update views.py

--- twizer/views.py
+++ twizer/views.py
@@ -7,17 +7,23 @@ from .utils.fileread import json_parse as parser
 
 def IndexView(request):
     form = DataForm()
-    if request.method == 'POST' and request.FILES['file']:	
-        filename = 'userfile'
-        file_obj = firstname= form.cleaned_data.get("file")
-        file = parser(file_obj)
-        filepath = filename + '.json'
-        with open(filepath, "w",encoding='utf8') as write_file:
-            json.dump(file, write_file)
-            response = HttpResponse(json.dumps(file), content_type='application/json')
-            response['Content-Disposition'] = "attachment; filename=%s" % filepath
+    if request.method == 'POST' and request.FILES['file']:
+        objects = DataForm(request.POST, request.FILES)
+        if objects.is_valid():
+            filename = objects.data.get('filename')
+            file_obj = objects.files.get('file')
+            print(file_obj)
+            try:
+                file = parser(file_obj)
+            except Exception as e:
+                raise e
+            filepath = filename + '.json'
+            with open(filepath, "w",encoding='utf8') as write_file:
+                json.dump(file, write_file)
+                response = HttpResponse(json.dumps(file), content_type='application/json')
+                response['Content-Disposition'] = "attachment; filename=%s" % filepath
                 # Return the response value
-            return response
+                return response
     else:
         form = DataForm()
     return render(request, 'index.html', {'form': form})
